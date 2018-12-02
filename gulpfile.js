2f2f5b878985bcf424c7a5412bf58de5ad540ab2
angelmer <maximer@mail.ru>
2018-12-02 11:34:12 +0300

    project is created

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function(){
+    return gulp.src('./less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./css'))
+
+});
+
+gulp.task('watch', function(){
+    return gulp.watch('./**/*.less', ['less']);
+
+});
+
+gulp.task('default', ['less' ,'watch']);
