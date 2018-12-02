cb79456d8c3a6986b5f66ca81aca08618374a530
angelmer <maximer@mail.ru>
2018-12-02 15:44:45 +0300

    change the gulpfile set browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let less = require('gulp-less');
+let browserSync = require('browser-sync');
+let plumber = require('gulp-plumber');
+let postCSS = require('gulp-postcss');
+let autoprefixer = require("autoprefixer");
 
 gulp.task('less', function(){
-    return gulp.src('./less/style.less')
+    gulp.src('./less/style.less')
+    .pipe(plumber())
     .pipe(less())
+    .pipe(postCSS([
+        autoprefixer()
+    ]))
     .pipe(gulp.dest('./css'))
-
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(){
-    return gulp.watch('./**/*.less', ['less']);
+gulp.task("server", ["less"], function() {
+    browserSync.init({
+      server: ".",
+      notify: false,
+      open: true,
+      cors: true,
+      ui: false
+    });
 
+    gulp.watch("less/**/*.less", ["less"]);
+    gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task('minify-css', function(){
+    return gulp.src('./css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'))
+    
+    
 });
 
 gulp.task('default', ['less' ,'watch']);
